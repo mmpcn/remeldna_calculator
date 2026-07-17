dd3839ccdd54a0ad9a7f6d84d9cdaf81b83d5d32
mmpcn <mcneves@ari.uni-heidelberg.de>
2026-07-17 17:00:48 +0200

    fix flutter gradle version properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-	versionCode = flutter.versionCode()
-	versionName = flutter.versionName()
+	versionCode = flutter.versionCode
+	versionName = flutter.versionName
     }
     signingConfigs {
         create("release") {
